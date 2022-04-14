cf807847c743b9b43842bfc2130eecea6691845c
Batuhan Akca <69108754+batuhanakca@users.noreply.github.com>
2022-04-14 15:31:25 +0200

    Create main.tf

---
+++ gke-basic-terraform/main.tf
@@ -0,0 +1,56 @@
+# NETWORK
+# VPC
+resource "google_compute_network" "vpc" {
+  name                    = "${var.cluster_name}-vpc"
+  auto_create_subnetworks = "false"
+}
+
+# Subnet
+resource "google_compute_subnetwork" "subnet" {
+  name          = "${var.cluster_name}-subnet"
+  region        = var.region
+  network       = google_compute_network.vpc.name
+  ip_cidr_range = "10.10.0.0/24"
+}
+
+# COMPUTE
+# GKE cluster 
+resource "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = var.region
+  
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  network    = google_compute_network.vpc.name
+  subnetwork = google_compute_subnetwork.subnet.name
+}
+
+# Separately Managed Node Pool
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "${google_container_cluster.primary.name}-node-pool"
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+  node_count = var.gke_num_nodes
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    labels = {
+      env = var.cluster_name
+    }
+
+    machine_type = var.machine_type
+    disk_size_gb = var.disk_size_gb
+    tags         = ["gke-node", var.cluster_name]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+}
